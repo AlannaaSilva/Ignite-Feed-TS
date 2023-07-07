b0c7b57dbe2e6a5e6125c95fe3b82636c54903ee
Alanna Silva <alannacostas55@gmail.com>
2023-07-07 18:17:03 -0300

    configuracoes do deploy

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const posts: PostProps[] = [
   id: 1,
   author: {
     avatarUrl: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-    name: "Jõao Almeida",
+    name: "João Almeida",
     role: "Web Developer"
   },
   content: [
